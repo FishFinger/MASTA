db5739a16f9c343b7309cd9cc4f942110d4e8736
Thibaut Marmin <marminthibaut@gmail.com>
2012-11-16 20:06:32 +0100

    maj pres

--- presentation/1.php
+++ presentation/1.php
@@ -20,6 +20,37 @@
 	
 <div class="slide">
 	<h1>Problématique</h1>
+	<h3>Domaines d'application</h3>
+	<ul title="Énumération des différents types d'agents page 21 : http://www-desir.lip6.fr/~briot/cv/sommaire-ic2-sma01.pdf">
+		<li>Agent cognitif</li>
+		<li title="Par exemple : démons Unix">Agent logiciel</li>
+		<li>Agent mobile</li>
+		<li>Agent assistant</li>
+		<li>Agent robotique</li>
+		<li>Résolution distribuée de problèmes</li>
+		<li>Agent réactif</li>
+		<li>Vie artificielle</li>
+		<li>Simulation multi-agent</li>
+		<li>Agent de loisir</li>
+	</ul>
+	<h3>Enjeux</h3>
+	<ul>
+		<li style="font-weight:bold;color:green">Autonomie</li>
+		<li style="font-weight:bold;color:brown">Organisation collective</li>
+	</ul>
+
+	<h3>Notions</h3>
+	<ul>
+		<li title="1.3">Coordination
+<p>Allocation / Partage de ressources rares dans le but d'atteindre un but commun.</p></li>
+		<li>Négociation
+	<p title="page 40 http://www-desir.lip6.fr/~briot/cv/sommaire-ic2-sma01.pdf">Présence d'une forme de conflit qui doit être résolu de façon décentralisée par des agents égocentrés ayant une rationalité limitée et des informations incomplètes</p></li>
+		<li>Planification</li>
+	</ul>
+</div>
+
+<div class="slide">
+	<h1>Problématique</h1>
 	<ul style="text-decoration:line-through">
 		<li>Agents egocentrés</li>
 		<li>Maximiser sa propre utilité</li>
