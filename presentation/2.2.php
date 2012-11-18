bc46d9ee14e5ae98574898ef7191eb35d7361513
Thibaut Marmin <marminthibaut@gmail.com>
2012-11-18 21:27:41 +0100

    correction erreur image hierarchique

--- presentation/2.2.php
+++ presentation/2.2.php
@@ -13,8 +13,8 @@
 
 <div class="slide">
 	<h1>Allocation centralisée</h1>
-  <h2>Système hierarchique</h2>
 <img alt="" src="img/hierarchique.png" style="float:right;width:20em;margin-right:2em" />
+  <h2>Système hierarchique</h2>
   <ul>
     <li>Organisation fixe</li>
     <li>Système rigide</li>
