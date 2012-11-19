f89edf5bc42c1509e441a44b8d0aaa00d5d39ded
Clément SIPIETER <c.sipieter@gmail.com>
2012-11-19 23:29:29 +0100

    update presentation contractuel

--- presentation/2.3.2.php
+++ presentation/2.3.2.php
@@ -2,21 +2,94 @@
 	<h1>Allocation décentralisée</h1>
   <h2>Réseau contractuel <span class="bib">[Smith 1979]</span></h2>
   
-  <div class="incremental clem_style" style="background-color:white"> 
+  <div class="incremental" style="background-color:white"> 
     
-      <img src="img/contractuel_1.png" alt="" /> 
-      <img src="img/contractuel_2.png" alt="" /> 
-      <img src="img/contractuel_3.png" alt="" /> 
-      <img src="img/contractuel_4.png" alt="" /> 
+    <div style="background: none repeat scroll 0% 0% white;width: 100%;position: absolute;right: 1em;top: 3em;">
+      <img src="img/contractuel_1.png" alt="" style="float: right; width:25em; margin:2em;"/> 
 
-   
-	</div> 
-  
-      <ol>
+      <ol style="margin-left:3em;">
+       
+      </ol>
+    </div>
+ 
+    <div style="background: none repeat scroll 0% 0% white;width: 100%;position: absolute;right: 1em;top: 3em;">
+            <img src="img/contractuel_2.png" alt="" style="float: right; width:25em; margin:2em;"/> 
+
+      <ol style="margin-left:3em;">
+        <li>Appel d'offres</li>
+       
+      </ol>
+    </div>
+    
+     <div style="background: none repeat scroll 0% 0% white;width: 100%;position: absolute;right: 1em;top: 3em;">
+            <img src="img/contractuel_3.png" alt="" style="float: right; width:25em; margin:2em;"/> 
+
+      <ol style="margin-left:3em;">
+        <li>Appel d'offres</li>
+        <li>Envoi de propositions</li>
+       
+      </ol>
+    </div>
+    <div style="background: none repeat scroll 0% 0% white;width: 100%;position: absolute;right: 1em;top: 3em;">
+            <img src="img/contractuel_4.png" alt="" style="float: right; width:25em; margin:2em;"/> 
+
+      <ol style="margin-left:3em;">
+        <li>Appel d'offres</li>
+        <li>Envoi de propositions</li>
+        <li>attribution du contrat</li>
+      </ol>
+    </div>
+     
+     <div style="background: none repeat scroll 0% 0% white;width: 100%;position: absolute;right: 1em;top: 3em;">
+            <img src="img/contractuel_5.png" alt="" style="float: right; width:25em; margin:2em;"/> 
+
+      <ol style="margin-left:3em;">
         <li>Appel d'offres</li>
         <li>Envoi de propositions</li>
         <li>attribution du contrat</li>
         <li>acquittement du contrat</li>
       </ol>
+    </div>
+     
+     
+     <div style="background: none repeat scroll 0% 0% white;width: 100%;position: absolute;right: 1em;top: 3em;">
+            <img src="img/contractuel_6.png" alt="" style="float: right; width:25em; margin:2em;"/> 
+
+      <ol style="margin-left:3em;">
+        <li>Appel d'offres</li>
+        <li>Envoi de propositions</li>
+        <li>attribution du contrat</li>
+      </ol>
+    </div>
+    
+     <div style="background: none repeat scroll 0% 0% white;width: 100%;position: absolute;right: 1em;top: 3em;">
+            <img src="img/contractuel_7.png" alt="" style="float: right; width:25em; margin:2em;"/> 
+
+      <ol style="margin-left:3em;">
+        <li>Appel d'offres</li>
+        <li>Envoi de propositions</li>
+        <li>attribution du contrat</li>
+        <li>acquittement du contrat</li>
+      </ol>
+    </div>
+     
+  <div class=" incremental">
+   <div class="popup spopup">
+
+      <ul class="positive" >
+        <li title="capacité d'adaptation au variation environnemental" >flexible</li>
+        <li title="temps, coût" >gestion de critères complexes</li>
+
+      </ul>
+      <ul class="negative">
+        <li>grande quantité de messages</li>
+      </ul>
+    </div>
+</div>
+
+   
+	</div> 
+  
+    
   
 </div>
