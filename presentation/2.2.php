090d2875a0483f657edd431fa51dc6fbfdbc4fb0
Thibaut Marmin <marminthibaut@gmail.com>
2012-11-19 12:14:52 +0100

    working on centralisé

--- presentation/2.2.php
+++ presentation/2.2.php
@@ -18,7 +18,7 @@
 </div>
 
 <div class="slide">
-	<h1>Allocation centralisée</h1>
+	<h1>Allocation centralisée - Système hierarchique</h1>
 <img alt="" src="img/hierarchique.png" style="float:right;width:20em;margin-right:2em" />
   <h2>Système hierarchique</h2>
   <h3>Fonctionnement</h3>
@@ -40,10 +40,48 @@
 </div>
 
 <div class="slide">
-	<h1>Allocation centralisée</h1>
+	<h1>Allocation centralisée - Système par médiateur</h1>
   <h2>Allocation par médiateur</h2>
-  <ul>
-    <li>Pôle emploi</li>
-  </ul>
-  
+
+	<div class="incremental" 
+	 style="margin: auto; position: relative; width:30em;"> 
+	  <img src="img/mediateur_anim/11systeme-mediateur.png" alt="" 
+	   style="position: static; vertical-align: bottom;width:30em"/> 
+	  <img src="img/mediateur_anim/10systeme-mediateur.png" alt="" 
+	    style="position: absolute; left: 0; top: 0;width:30em" /> 
+	  <img src="img/mediateur_anim/9systeme-mediateur.png" alt="" 
+	    style="position: absolute; left: 0; top: 0;width:30em" /> 
+	  <img src="img/mediateur_anim/8systeme-mediateur.png" alt="" 
+	    style="position: absolute; left: 0; top: 0;width:30em" /> 
+	  <img src="img/mediateur_anim/7systeme-mediateur.png" alt="" 
+	    style="position: absolute; left: 0; top: 0;width:30em" /> 
+	  <img src="img/mediateur_anim/6systeme-mediateur.png" alt="" 
+	    style="position: absolute; left: 0; top: 0;width:30em" /> 
+	  <img src="img/mediateur_anim/5systeme-mediateur.png" alt="" 
+	    style="position: absolute; left: 0; top: 0;width:30em" /> 
+	  <img src="img/mediateur_anim/4systeme-mediateur.png" alt="" 
+	    style="position: absolute; left: 0; top: 0;width:30em" /> 
+	  <img src="img/mediateur_anim/3systeme-mediateur.png" alt="" 
+	    style="position: absolute; left: 0; top: 0;width:30em" /> 
+	  <img src="img/mediateur_anim/2systeme-mediateur.png" alt="" 
+	    style="position: absolute; left: 0; top: 0;width:30em" /> 
+	  <img src="img/mediateur_anim/1systeme-mediateur.png" alt="" 
+	    style="position: absolute; left: 0; top: 0;width:30em" /> 
+	  <img src="img/mediateur_anim/0systeme-mediateur.png" alt="" 
+	    style="position: absolute; left: 0; top: 0;width:30em" /> 
+	</div>   
+</div>
+
+<div class="slide">
+  <h1>Allocation centralisée - Système par médiateur</h1>
+  <h2>Variantes</h2>
+  <p>Notion de niveau de compétence</p>
+  <div class="incremental">
+    <div style="float:left;width:48%;text-align:center"><h3>Évaluation des agents par le médiateur</h3>
+      <img alt="" src="img/table_competences_valuees.png" style="width:13em"/>
+    </div>
+    <div style="float:right;width:48%;text-align:center"><h3>Appel d'offre</h3>
+      <img alt="" src="img/megaphone.png" style="width:13em"/>
+    </div>
+  </div>
 </div>
