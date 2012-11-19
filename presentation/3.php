4b072ec3335c758a64b4498a7e21bf191737bfc0
Clément SIPIETER <c.sipieter@gmail.com>
2012-11-19 23:50:53 +0100

    premier jet diapo projet

--- presentation/3.php
+++ presentation/3.php
@@ -1,5 +1,22 @@
 <div class="slide">
 	<h1>Projet</h1>
-	
+
+  <img src="img/warcraft2.png" alt="" style="float: right; width:15em; margin:2em;"/> 
+
+   <ul>
+     <li>Quatre méthodes d'allocation de tâches :
+</li>
+     <ul>
+        <li>Réseau d'accointance</li>
+        <li>Appels d'offre</li>
+        <li>Hybride</li>
+        <li>Modèle à agent réactif</li>
+    </ul>
+     <li>Comparaison des Quatre méthodes dans deux situations :</li>
+     <ul>
+         <li>Sans acquisition de compétences</li>
+         <li>avec acquisition de compétences</li>
+        </ul>
+        </ul>
 </div>
 
