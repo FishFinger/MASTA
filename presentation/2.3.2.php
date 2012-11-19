5b4e25fd9623bde2f9eaee39945ff1cb3bcfc29d
Clément SIPIETER <c.sipieter@gmail.com>
2012-11-19 15:22:40 +0100

    update presentation 2.3.2

--- presentation/2.3.2.php
+++ presentation/2.3.2.php
@@ -1,7 +1,22 @@
 <div class="slide">
 	<h1>Allocation décentralisée</h1>
   <h2>Réseau contractuel [Smith 1979]</h2>
-  <ul>
-    <li>Appel d'offre</li>
-  </ul>
+  
+  <div class="incremental" style="background-color:white"> 
+    
+      <img src="img/contractuel_1.png" alt="" style="background:white; width:25em; position: absolute; right: 5em; top: 5em;" /> 
+      <img src="img/contractuel_2.png" alt="" style="background:white; width:25em; position: absolute; right: 5em; top: 5em;" /> 
+      <img src="img/contractuel_3.png" alt="" style="background:white; width:25em; position: absolute; right: 5em; top: 5em;" /> 
+      <img src="img/contractuel_4.png" alt="" style="background:white; width:25em; position: absolute; right: 5em; top: 5em;" /> 
+
+   
+	</div> 
+  
+      <ol>
+        <li>Appel d'offres</li>
+        <li>Envoi de propositions</li>
+        <li>attribution du contrat</li>
+        <li>acquittement du contrat</li>
+      </ol>
+  
 </div>
